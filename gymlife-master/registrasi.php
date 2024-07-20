2808294553559067812208132dcf24d2074d8b0e
mighdad <mr.mighdadjaba@gmail.com>
2024-07-20 21:10:19 +0800

    registrasi

--- gymlife-master/registrasi.php
+++ gymlife-master/registrasi.php
@@ -18,7 +18,7 @@ include "koneksi.php";
   <section class="bg-white">
     <div class="lg:grid lg:min-h-screen lg:grid-cols-12">
       <aside class="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
-        <img alt="gambar gym" src="gambar.jpg" class="absolute inset-0 h-full w-full object-cover" />
+        <img alt="gambar gym" src="img/hero/hero-1.jpg" class="absolute inset-0 h-full w-full object-cover" />
       </aside>
 
       <main class="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
